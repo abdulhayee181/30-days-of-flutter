7af4bd37e5c4902b8aedfdd81740aa321807e497
abdulhayee181 <hayee181@gmail.com>
2021-08-08 22:32:49 -0700

    first commit

---
+++ lib/home_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget{
+  final int days =40;
+   Widget build(BuildContext context){
+   return Scaffold(
+     appBar: AppBar(
+       title: Text("myapp"),
+     ),
+        body: Center(
+          child: Container(
+            child:Text("welcometo $days days of flutter"),
+          ),
+        ),
+        drawer: Drawer(),
+      ),
+   }
+}
