f06c252d5d98cd37cf818fbcbda8c589be9a77ee
abdulhayee181 <hayee181@gmail.com>
2021-08-19 10:35:35 -0700

    dataDecode

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,12 +1,33 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter/services.dart';
 import 'package:hello_world/models/catalog.dart';
 import 'package:hello_world/widgets/drawer.dart';
 import 'package:hello_world/widgets/item_widget.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   final int days = 40;
   final String imageurl =
       "https://itchronicles.com/wp-content/uploads/2021/01/technology-impact-on-life-1024x566.jpg";
+@override
+  void initState() {
+    super.initState();
+    loadData();
+  }
+  loadData() async{
+   final catalogjson = await rootBundle.loadString("assests/files/catalog.json");
+  final decodedata = jsonDecode(catalogjson);
+  var productData = decodedata ["products"];
+  print(productData);
+  }
+@override     
   Widget build(BuildContext context) {
     final dummylist = List.generate(10, (index) => CatalogModel.items[0]);
     return Scaffold(
